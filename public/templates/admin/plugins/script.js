0b4663d255043c53715b3df18bcc201e3e4044c7
pichalite <pichalite.com@gmail.com>
2015-05-31 10:11:58 +0530

    updated for NodeBB v0.7.x

--- public/templates/admin/plugins/script.js
+++ public/templates/admin/plugins/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+var nonApprovedGroupName,
+    approvedGroupName;
+
+    socket.emit('admin.settings.get', {
+            hash: 'newuser-approval' }, function(err, values) { 
+                if (err) {
+                    console.log('Unable to load settings');
+                    
+                } else {
+                    nonApprovedGroupName = values.nonapprovedUserGroup;
+                    approvedGroupName = values.approvedUserGroup;
+                }
+
+    });
+
 require(['settings'], function(Settings) {
     Settings.load('newuser-approval', $('.newuser-approval-settings'));
 
@@ -20,14 +35,12 @@ require(['settings'], function(Settings) {
 });
 
 $('#users-container').on('click', 'div[data-uid]', function() {
-    var uid = $(this).attr('data-uid'),
-        nonApprovedGroupName = $('#nonapprovedUserGroup').val(),
-        approvedGroupName = $('#approvedUserGroup').val();
+    var uid = $(this).attr('data-uid');
     socket.emit('admin.groups.join', { 
                 groupName: approvedGroupName,
                 uid: uid
             }, function(err, data) {
-                if (!err){
+                if (!err) {
                     socket.emit('admin.groups.leave', {
                         groupName: nonApprovedGroupName,
                         uid: uid
@@ -42,18 +55,28 @@ $('#users-container').on('click', 'div[data-uid]', function() {
 
 $(document).ready(function() {
     $(window).on('action:ajaxify.end', function(event, data) {
-       var nonApprovedGroupName = $('#nonapprovedUserGroup').val();
-        socket.emit('admin.groups.get', nonApprovedGroupName, function(err, data) {
-            if (data.members.length > 0) {
-                for (var x = 0; x < data.members.length; x++) {
-                    var html = $('<div />')
-                        .attr('class', 'users-box')
-                        .append($('<div />').attr('class', 'user-image').attr('data-uid', data.members[x].uid)
-                        .attr('data-username', data.members[x].username).append($('<img />').attr('src', data.members[x].picture).attr('class', 'img-thumbnail user-selectable selection')))
-                        .append($('<a />').attr('href', '/user/'+data.members[x].username).attr( 'target', '_blank').html(data.members[x].username));
-                    $('#users-container').append(html);
-                }
-            }
+
+        socket.emit('admin.settings.get', {
+                hash: 'newuser-approval' }, function(err, values) { 
+                    if (err) {
+                        console.log('Unable to load settings');
+                        
+                    } else {
+                        socket.emit('admin.groups.get', values.nonapprovedUserGroup, function(err, data) {
+                            $('#users-container').empty();
+                            if (data.members.length > 0) {
+                                for (var x = 0; x < data.members.length; x++) {
+                                    var html = $('<div />')
+                                        .attr('class', 'users-box')
+                                        .append($('<div />').attr('class', 'user-image').attr('data-uid', data.members[x].uid)
+                                        .attr('data-username', data.members[x].username).append($('<img />').attr('src', data.members[x].picture).attr('class', 'img-thumbnail user-selectable selection')))
+                                        .append($('<a />').attr('href', '/user/'+data.members[x].username).attr( 'target', '_blank').html(data.members[x].username));
+                                    $('#users-container').append(html);
+                                }
+                            }
+                        });
+                    }
+
         });
     });
 });
